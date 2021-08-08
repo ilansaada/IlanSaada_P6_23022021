6dc7be9a408dea22c1b5743f858ac961971fb378
ilan saada <ilansaada24@gmail.com>
2021-08-08 21:00:24 +0200

    création de toutes les routes

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+/*----------------------------------importation du routeur-----------------------------*/
+const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauces');
+
+
+/*----------------------------------Connexion  à la base de données-----------------------------*/
+mongoose.connect("mongodb+srv://IlanSaada:IlanSaada24@cluster0.byflg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log("Connexion à MongoDB réussie !"))
+.catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+/*-------------------------------------------CORS------------------------------------------*/
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
+
+
+
+
+module.exports = app;
